a9e8d34ca18a7805ca08c222313cd19b0c60ace3
glebchik230 <gyfi228sidan@gmail.com>
2025-07-20 23:44:39 +0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,4 @@
+const SUPABASE_URL = 'https://jmsqhicputuowgmgrfjo.supabase.co';
+const SUPABASE_ANON_KEY = 'твой_anon_key';
+
+const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
