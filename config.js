13f8cda58480f215d7bf5d5118edfbf7c3a2188d
glebchik230 <gyfi228sidan@gmail.com>
2025-07-21 18:24:09 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
+// config.js — подключение Supabase
 const SUPABASE_URL = 'https://jmsqhicputuowgmgrfjo.supabase.co';
-const SUPABASE_ANON_KEY = 'твой_anon_key';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imptc3FoaWNwdXR1b3dnbWdyZmpvIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc1MzAzNDg0MywiZXhwIjoyMDY4NjEwODQzfQ.q1nhYlKpu1zhbq9Q7SVZIj0ZynofdOKzlXyNqLd-AS4';
 
 const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
